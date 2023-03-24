d5870a5c940485ddc928a806b64bce37c7ffd408
mehtab <mehtabsmd@gmail.com>
2023-03-23 22:12:37 -0400

    learned if else condition

--- MyFirstGithubProject/src/main/java/GitHub.java
+++ MyFirstGithubProject/src/main/java/GitHub.java
@@ -1,7 +1,33 @@
 public class GitHub {
     public static void main(String[] args)
     {
-        System.out.println("my 1st github project");
+
+       // System.out.println(canFly(true));
+       // System.out.println(canFly(false));
+        isEven(3);
+    }
+    public static int getProduct(int a,int b)
+    {
+        return a * b ;
+    }
+    public static boolean canFly(boolean hasWings) {
+        if (hasWings == true) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+        public static void isEven(int n)
+        {
+            if(n%2 == 1)
+            {
+                System.out.println("true");
+            }
+            else
+                System.out.println("false");
+                   // System.out.println("false");
+
+        }
     }
 
-}
+
